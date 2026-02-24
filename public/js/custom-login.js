4facfbb9903415663b5a878365036b41459fa3ec
Henrik Enger Milo <hanas879@milo.life>
2026-02-24 15:53:54 +0100

    Make style apply to "forgot password" page

--- public/js/custom-login.js
+++ public/js/custom-login.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function() {
     if (form && back) form.appendChild(back);
     if (form && lang) form.appendChild(lang);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById('lostpasswordform');
+    var nav = document.getElementById('nav');
+    var back = document.getElementById('backtoblog');
+    var lang = document.getElementById('language-switcher');
+
+    // Move the links inside the form element at the end
+    if (form && nav) form.appendChild(nav);
+    if (form && back) form.appendChild(back);
+    if (form && lang) form.appendChild(lang);
+});
