b242f7e6b4a6dbe8132f26819ec6fa1fc7a66858
Henrik Enger Milo <hanas879@milo.life>
2026-02-24 15:24:54 +0100

    move css and js to separate files

--- custom-login.php
+++ custom-login.php
@@ -7,113 +7,27 @@ Author: Agreed AS
 */
 
 add_action('login_enqueue_scripts', function() {
-    ?>
-    <style type="text/css">
-        #login h1 a, .login h1 a {
-            background-image: url('https://media.agreed.no/agreed-t-n-white.png');
-            height: 80px;
-            width: 320px;
-            background-size: contain;
-            background-repeat: no-repeat;
-	}
 
-        body.login { background: #000; }
-	.wp-core-ui .button-primary { background: #0073aa; border-color: #006799; }
+	wp_enqueue_style(
+		'Agreed Custom Login Theme',
+		plugins_url('public/css/custom-login.css', __FILE__),
+		array(),
+		'1.0'
+	);
 
-	#login-message {
-		border-left: 4px solid #264EB5;
-	}
-	
-	.login #login {
-		width: 450px;
-	}
-
-	#loginform {
-		display: flex;
-		flex-wrap: wrap;
-		border-radius: 10px;
-	}
-
-	.user-pass-wrap {
-		width: 100%;
-	}
-
-	#user_pass:focus, #user_login:focus {
-		border-color: #264EB5;
-	}
-
-	.dashicons{
-		color: #264EB5;
-	}
-	
-	#loginform .submit {
-		width: 100%;
-	}
-	
-	#loginform .submit .button {
-		width: 100%;
-		margin-top: 1rem;
-		background: #264EB5;
-	}
-	
-	#loginform .submit .button:hover {
-		opacity: 0.9;
-	}
-
-	#nav {
-		order: 11
-	}
-
-	#backtoblog {
-		order: 10
-	}
-
-	#nav, #backtoblog {
-		margin-top: 1rem !important;
-	}
-
-	#language-switcher {
-		width: 100%;
-		display: flex;
-		flex-wrap: nowrap;
-		align-items: center;
-		justify-content: center;
-		gap: 0.4rem;
-		order: 12;
-	}
-
-	#language-switcher select:hover {
-		color: #264EB5;
-	}
-
-	#language-switcher input {
-		border-color: #264EB5;
-		color: #264EB5;
-	}
-
-    </style>
-	<script type="text/javascript">
-        	document.addEventListener("DOMContentLoaded", function() {
-            		var form = document.getElementById('loginform');
-            		var nav = document.getElementById('nav');
-            		var back = document.getElementById('backtoblog');
-            		var lang = document.getElementById('language-switcher');
-
-            		// Move the links inside the form element at the end
-            		if (form && nav) form.appendChild(nav);
-            		if (form && back) form.appendChild(back);
-            		if (form && lang) form.appendChild(lang);
-        	});
-    	</script>
-    <?php
+	wp_enqueue_script(
+		'Agreed Custom Login Theme',
+		plugins_url('public/js/custom-login.js', __FILE__),
+		array('jquery'),
+		'1.0',
+		true
+	);
 });
 
-// 2. Change the Logo Link (defaults to WordPress.org)
 add_filter('login_headerurl', function() {
     return home_url();
 });
 
-// 3. Change the Logo Title (alt text)
 add_filter('login_headertext', function() {
     return get_bloginfo('name');
 });
---
+++ public/js/custom-login.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.getElementById('loginform');
+    var nav = document.getElementById('nav');
+    var back = document.getElementById('backtoblog');
+    var lang = document.getElementById('language-switcher');
+
+    // Move the links inside the form element at the end
+    if (form && nav) form.appendChild(nav);
+    if (form && back) form.appendChild(back);
+    if (form && lang) form.appendChild(lang);
+});
