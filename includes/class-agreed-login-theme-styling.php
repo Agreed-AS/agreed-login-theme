d9a30b0876c11384405d28a875aa9d48451b4aba
Henrik Enger Milo <hanas879@milo.life>
2026-02-25 16:29:01 +0100

    change code to use classes and add some comments

---
+++ includes/class-agreed-login-theme-styling.php
@@ -0,0 +1,39 @@
+<?php
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class AgreedLoginThemeStyling {
+    public function __construct() {
+        add_action('login_enqueue_scripts', array($this, 'style_login'));
+    }
+
+    //Loads the correct CSS file to style the wp-login.php site
+    public function style_login() {
+        wp_enqueue_style(
+            'Agreed Custom Login Theme',
+            PLUGIN_URL . 'public/css/custom-login.css',
+            array(),
+            '1.0'
+        );
+
+        //Loads the correct JS file to activate some logic for the wp-login.php site
+        wp_enqueue_script(
+            'Agreed Custom Login Theme',
+            PLUGIN_URL . 'public/js/custom-login.js',
+            array('jquery'),
+            '1.0',
+            true
+        );
+
+        //Used for changing the URL of the logo on top, defaults to the site homepage
+        add_filter('login_headerurl', function() {
+            return home_url();
+        });
+
+        add_filter('login_headertext', function() {
+            return get_bloginfo('name');
+        });
+    }
+}
