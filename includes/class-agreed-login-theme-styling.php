51835bb15d008b3d92c1e98c09b3c19862ad17aa
Henrik Enger Milo <hanas879@milo.life>
2026-02-26 08:43:34 +0100

    add basic button and functionality for switching CSS

--- includes/class-agreed-login-theme-styling.php
+++ includes/class-agreed-login-theme-styling.php
@@ -12,15 +12,25 @@ class AgreedLoginThemeStyling {
     //Loads the correct CSS file to style the wp-login.php site
     public function style_login() {
         wp_enqueue_style(
-            'Agreed Custom Login Theme',
+            'agreed-custom-theme',
             PLUGIN_URL . 'public/css/custom-login.css',
             array(),
             '1.0'
         );
 
+        $theme_mode = get_option( 'agreed_theme_color');
+        if ($theme_mode !== 'dark') {
+            $light_vars = "
+                :root {
+                    --background_color: #fff !important;
+                }
+            ";
+            wp_add_inline_style( 'agreed-custom-theme', $light_vars );
+        }
+
         //Loads the correct JS file to activate some logic for the wp-login.php site
         wp_enqueue_script(
-            'Agreed Custom Login Theme',
+            'agreed-custom-theme',
             PLUGIN_URL . 'public/js/custom-login.js',
             array('jquery'),
             '1.0',
