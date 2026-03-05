e30996403ba22c1a2db101a1dca49452faf43b98
Henrik Enger Milo <hanas879@milo.life>
2026-03-05 14:27:26 +0100

    implement custom logo feature and change some logic for dark/light mode

--- includes/class-agreed-login-theme-settings.php
+++ includes/class-agreed-login-theme-settings.php
@@ -23,8 +23,12 @@ class AgreedLoginThemeSetting {
     }
 
     public function init_settings_menu() {
-        //Creates a option/setting group and name. 
+        //Creates a option/setting group and name.
         register_setting('agreed_login_theme_settings', 'agreed_theme_color');
+        register_setting('agreed_login_theme_settings', 'agreed_theme_custom_logo', array(
+            'type' => 'string',
+            'sanitize_callback' => 'esc_url_raw',
+        ));
     }
 
     //Creates the HTML for the theme menu
@@ -32,21 +36,40 @@ class AgreedLoginThemeSetting {
         ?>
         <div class="wrap">
         <h1>Agreed Login Theme Settings</h1>
-        <!--Creates the button/switch used to toggle light/dark mode-->
+       <!--Setting up the form post for WordPress to handle the options for the plugin-->
         <form method="post" action="options.php">
             <?php
             settings_fields('agreed_login_theme_settings');
             $theme_mode = get_option('agreed_theme_color', 'dark');
+            $theme_logo = get_option('agreed_theme_custom_logo', '');
             ?>
-            
+
+             <!--Creates the button/switch used to toggle light/dark mode-->
             <table class="form-table">
                 <tr>
-                    <th scope="row">Dark background</th>
+                    <th scope="row"><label for="color_switch">Dark background</label></th>
                     <td>
                         <label class="switch">
-                            <input type="checkbox" name="agreed_theme_color" value="dark" <?php checked('dark', $theme_mode); ?>>
+                            <input type="hidden" name="agreed_theme_color" value="light">
+                            <input id="color_switch" type="checkbox" name="agreed_theme_color" value="dark" <?php checked('dark', $theme_mode); ?>>
                             <span class="slider round"></span>
                         </label>
+                    </td>
+                </tr>
+            </table>
+
+            <!--Creates the input for the custom logo URL-->
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="logo-url-input">Custom Logo URL</label></th>
+                    <td>
+                        <input id="logo-url-input" type="url" name="agreed_theme_custom_logo" value="<?php echo esc_url($theme_logo)?>"></input>
+                        
+                        <div id="logo-preview-wrap" style="margin-top: 15px; max-width: 300px; padding: 10px; border: 1px dashed #ccc; background: #f9f9f9; display: <?php echo $theme_logo ? 'block' : 'none'; ?>;">
+                            <p style="margin-top:0; font-size: 11px; text-transform: uppercase; color: #666;">Preview:</p>
+                            <br>
+                            <img id="logo-preview-img" src="<?php echo esc_url($theme_logo); ?>" style="max-width: 100%; height: auto; display: block;">
+                        </div>
                     </td>
                 </tr>
             </table>
@@ -65,5 +88,10 @@ class AgreedLoginThemeSetting {
     input:checked + .slider:before { transform: translateX(26px); }
     </style>
         <?php
+        //DEBUG
+        echo 'Theme color: ' . get_option('agreed_theme_color');
+        echo '<br>';
+        echo 'Theme logo: ' . get_option('agreed_theme_custom_logo');
+        //END OF DEBUG
     }
 }
