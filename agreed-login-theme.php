b5a439ccfbcf16f6eac524c749844292b941f5dc
Henrik Enger Milo <hanas879@milo.life>
2026-03-05 15:38:54 +0100

    remove testing and update plugin to v2

--- agreed-login-theme.php
+++ agreed-login-theme.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Agreed Custom Login Theme
 Description: Custom login theme for websites made by Agreed AS
-Version: 1.8
+Version: 2.0
 Author: Agreed AS
 Author URI: https://agreed.no
 */
--- includes/class-agreed-login-theme-settings.php
+++ includes/class-agreed-login-theme-settings.php
@@ -88,6 +88,5 @@ class AgreedLoginThemeSetting {
     input:checked + .slider:before { transform: translateX(26px); }
     </style>
         <?php
-        echo "<p>testing!</p>";
     }
 }
