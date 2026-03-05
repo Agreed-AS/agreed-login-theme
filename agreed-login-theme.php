dbe83cc05ab09bc11a8a81efe6a101f1a8b53d9d
Henrik Enger Milo <hanas879@milo.life>
2026-03-05 15:16:48 +0100

    bump version

--- agreed-login-theme.php
+++ agreed-login-theme.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Agreed Custom Login Theme
 Description: Custom login theme for websites made by Agreed AS
-Version: 1.2
+Version: 1.6
 Author: Agreed AS
 Author URI: https://agreed.no
 */
